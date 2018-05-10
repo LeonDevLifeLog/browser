7b41b266b55e28c0477341ab944c2445a48cc017
Leon <leondevlifelog@gmail.com>
2018-05-10 23:34:49 +0800

    删除未使用到的资源

--- app/src/main/java/com/github/leondevlifelog/browser/MyWebChromeClient.kt
+++
@@ -1,6 +0,0 @@
-package com.github.leondevlifelog.browser
-
-import android.webkit.WebChromeClient
-
-class MyWebChromeClient : WebChromeClient() {
-}
--- app/src/main/java/com/github/leondevlifelog/browser/MyWebViewClient.kt
+++
@@ -1,5 +0,0 @@
-package com.github.leondevlifelog.browser
-
-import android.webkit.WebViewClient
-
-class MyWebViewClient : WebViewClient()
